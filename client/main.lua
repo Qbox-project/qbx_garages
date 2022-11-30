346277672c8de16e40d84d74e562dccf6f5beb42
adweex <adweex@gmail.com>
2022-11-30 20:48:43 +0100

    Now it will really damage the cars Visually

--- client/main.lua
+++ client/main.lua
@@ -200,7 +200,7 @@ local function doCarDamage(currentVehicle, veh)
     local body = veh.body + 0.0
     local data = json.decode(veh.mods)
 
-    if Config.VisuallyDamageCars then
+    if VisuallyDamageCars then
         for k, v in pairs(data.doorStatus) do
             if v then
                 SetVehicleDoorBroken(currentVehicle, tonumber(k), true)
