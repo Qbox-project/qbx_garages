23b0abb276cc9e187a4f111a0bb8894f4b602469
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2023-02-27 03:48:54 +0530

    fix(client.lua): <br> not supported in ox_lib (#5)

--- client/main.lua
+++ client/main.lua
@@ -108,7 +108,7 @@ local function CreateZone(type, garage, index)
                 if garage.type == "house" then
                     text = Lang:t("info.park_e")
                 else
-                    text = Lang:t("info.park_e") .. "<br>" .. garage.label
+                    text = Lang:t("info.park_e") .. "  \n" .. garage.label
                 end
                 exports['qb-core']:DrawText(text, 'left')
                 InputIn = true
@@ -116,7 +116,7 @@ local function CreateZone(type, garage, index)
                 if garage.type == "house" then
                     text = Lang:t("info.car_e")
                 else
-                    text = Lang:t("info." .. garage.vehicle .. "_e") .. "<br>" .. garage.label
+                    text = Lang:t("info." .. garage.vehicle .. "_e") .. "  \n" .. garage.label
                 end
 
                 exports['qb-core']:DrawText(text, 'left')
