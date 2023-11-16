a0b739e8b4fba5a538ef7af7e794e16a0ffa68bd
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 10:12:13 -0500

    fix: import modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-version '1.0.0'
+description 'QBX_Garages'
 repository 'https://github.com/Qbox-project/qbx_garages'
+version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@qbx_core/import.lua',
+    '@qbx_core/modules/utils.lua',
     '@qbx_core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
@@ -14,17 +15,13 @@ shared_scripts {
 }
 
 client_scripts {
+    '@qbx_core/modules/playerdata.lua',
     'client/main.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
-}
-
-modules {
-    'qbx_core:playerdata',
-    'qbx_core:utils',
 }
 
 dependency 'ox_lib'
