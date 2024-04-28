64c73c41f46fced06d2c98c844c442ebe0729428
Manason <clanerp@gmail.com>
2024-04-28 00:32:56 -0700

    refactor: VehicleType and GarageType enums
    * refactor: VehicleType and GarageType enums
    
    * fix missing brace

--- shared/types.lua
+++ shared/types.lua
@@ -3,3 +3,19 @@ VehicleState = {
     GARAGED = 1,
     IMPOUNDED = 2
 }
+
+---@enum VehicleType
+VehicleType = {
+    CAR = 'car',
+    AIR = 'air',
+    SEA = 'sea',
+    ALL = 'all',
+}
+
+---@enum GarageType
+GarageType = {
+    PUBLIC = 'public',
+    JOB = 'job',
+    GANG = 'gang',
+    DEPOT = 'depot',
+}
