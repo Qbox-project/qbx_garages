a46229ee432b614c9256ffb31f53dbcd1bf5e949
Manason <clanerp@gmail.com>
2024-06-24 08:35:15 -0700

    refactor!: move shared garages as a field of a garage instead of a global config value

--- config/server.lua
+++ config/server.lua
@@ -1,7 +1,5 @@
 return {
     autoRespawn = false, -- True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
-    sharedGarages = false, -- True == Gang and job garages are shared, false == Gang and Job garages are personal
-
     impoundFee = {
         enable = true, -- If true, impound fee is calculated and applied. If false, no impound fee is applied and depotprice remains at 0
 
