7a884787b511db51585c68c86349440b0fa611fd
Manason <clanerp@gmail.com>
2024-06-26 06:13:07 -0700

    fix(config): typo preventing warping (#79)

--- config/shared.lua
+++ config/shared.lua
@@ -2,7 +2,7 @@ require 'shared.types'
 
 return {
     takeOut = {
-        warpInvehicle = false, -- If false, player will no longer warp into vehicle upon taking the vehicle out.
+        warpInVehicle = false, -- If false, player will no longer warp into vehicle upon taking the vehicle out.
         doorsLocked = true, -- If true, the doors will be locked upon taking the vehicle out.
         engineOff = true, -- If true, the engine will be off upon taking the vehicle out.
     },
