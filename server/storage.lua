d87497c26edea383e797c13fe9e9ff786a144a0c
Manason <clanerp@gmail.com>
2024-06-27 06:37:59 -0700

    feat: new exports to transfer garages and change the depot price

--- server/storage.lua
+++ server/storage.lua
@@ -19,8 +19,33 @@ local function setVehicleStateToOut(vehicleId, depotPrice)
     MySQL.update('UPDATE player_vehicles SET state = ?, depotprice = ? WHERE id = ?', {state, depotPrice, vehicleId})
 end
 
+---@param vehicleId integer
+---@param garageName string
+---@param state VehicleState
+---@return integer numRowsAffected
+local function setVehicleGarage(vehicleId, garageName, state)
+    return MySQL.update('UPDATE player_vehicles SET garage = ? state = ? WHERE id = ?', {
+        garageName,
+        state,
+        vehicleId
+    })
+end
+
+---@param vehicleId integer
+---@param depotPrice integer
+---@return integer numRowsAffected
+local function setVehicleDepotPrice(vehicleId, depotPrice)
+    return MySQL.update('UPDATE player_vehicles SET depotPrice = ? WHERE id = ? AND state != ?', {
+        depotPrice,
+        vehicleId,
+        VehicleState.GARAGED
+    })
+end
+
 return {
     saveVehicle = saveVehicle,
     moveOutVehiclesIntoGarages = moveOutVehiclesIntoGarages,
     setVehicleStateToOut = setVehicleStateToOut,
+    setVehicleGarage = setVehicleGarage,
+    setVehicleDepotPrice = setVehicleDepotPrice,
 }
