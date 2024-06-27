b1bdfd94fc06d3cfd86fad99e3b94af98557c028
Matthew <22198949+mafewtm@users.noreply.github.com>
2024-06-27 17:21:51 -0400

    fix(config/server): garage blips not showing (#92)

--- config/server.lua
+++ config/server.lua
@@ -41,6 +41,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Motel Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(275.58, -344.74, 45.17, 70.0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(271.26, -342.32, 44.7, 159.97),
@@ -52,6 +57,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'San Andreas Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(-330.67, -781.12, 33.96, 40.46),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-337.11, -775.34, 33.56, 132.09),
@@ -63,6 +73,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Spanish Ave Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(-1160.46, -741.04, 19.95, 41.26),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-1165.38, -747.65, 18.94, 40.45),
@@ -74,6 +89,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Caears 24 Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(68.08, 13.15, 69.21, 160.44),
                     size = vec3(10, 10, 10),
                     spawn = vec4(72.61, 11.72, 68.47, 157.59),
@@ -85,6 +105,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Little Seoul Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(-463.51, -808.2, 30.54, 0.0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-472.24, -813.61, 30.3, 179.88),
@@ -96,6 +121,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Laguna Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(363.85, 297.97, 103.5, 341.39),
                     size = vec3(10, 10, 10),
                     spawn = vec4(367.41, 297.02, 103.2, 341.08),
@@ -107,6 +137,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Airport Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(-796.07, -2023.26, 9.17, 55.18),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-793.35, -2020.62, 8.51, 58.42),
@@ -118,6 +153,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Beach Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(-1184.21, -1509.65, 4.65, 303.72),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-1184.4, -1501.88, 4.39, 214.7),
@@ -129,6 +169,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'The Motor Hotel Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(1137.77, 2663.54, 37.9, 0.0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(1137.56, 2674.19, 38.17, 359.95),
@@ -140,6 +185,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Liqour Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(960.68, 3609.32, 32.98, 268.97),
                     size = vec3(10, 10, 10),
                     spawn = vec4(960.48, 3605.71, 32.98, 87.09),
@@ -151,6 +201,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Shore Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(1726.9, 3710.38, 34.26, 22.54),
                     size = vec3(10, 10, 10),
                     spawn = vec4(1728.65, 3714.85, 34.18, 21.26),
@@ -162,6 +217,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Bell Farms Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(78.34, 6418.74, 31.28, 0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(70.71, 6425.16, 30.92, 68.5),
@@ -173,6 +233,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Dumbo Private Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(157.26, -3240.00, 7.00, 0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(165.32, -3236.10, 5.93, 268.5),
@@ -184,6 +249,11 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Pillbox Garage Parking',
+                        sprite = 357,
+                        color = 3,
+                    },
                     coords = vec4(218.66, -804.08, 30.75, 65.69),
                     size = vec3(10, 10, 10),
                     spawn = vec4(229.33, -805.01, 30.54, 156.79),
@@ -195,13 +265,14 @@ return {
             vehicleType = VehicleType.AIR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Airport Hanger',
+                        sprite = 360,
+                        color = 3,
+                    },
                     coords = vec4(-1025.34, -3017.0, 13.95, 331.99),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-979.2, -2995.51, 13.95, 52.19),
-                    blip = {
-                        name = 'Hanger',
-                        sprite = 360,
-                    },
                 }
             },
         },
@@ -210,13 +281,14 @@ return {
             vehicleType = VehicleType.AIR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Higgins Helitours',
+                        sprite = 360,
+                        color = 3,
+                    },
                     coords = vec4(-722.12, -1472.74, 5.0, 140.0),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-724.83, -1443.89, 5.0, 140.0),
-                    blip = {
-                        name = 'Hanger',
-                        sprite = 360,
-                    },
                 }
             },
         },
@@ -225,13 +297,14 @@ return {
             vehicleType = VehicleType.AIR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Sandy Shores Hangar',
+                        sprite = 360,
+                        color = 3,
+                    },
                     coords = vec4(1757.74, 3296.13, 41.15, 142.6),
                     size = vec3(10, 10, 10),
                     spawn = vec4(1740.88, 3278.99, 41.09, 189.46),
-                    blip = {
-                        name = 'Hanger',
-                        sprite = 360,
-                    },
                 }
             },
         },
@@ -240,13 +313,14 @@ return {
             vehicleType = VehicleType.SEA,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'LSYMC Boathouse',
+                        sprite = 356,
+                        color = 3,
+                    },
                     coords = vec4(-794.64, -1510.89, 1.6, 201.55),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-793.58, -1501.4, 0.12, 111.5),
-                    blip = {
-                        name = 'Boathouse',
-                        sprite = 356,
-                    },
                 }
             },
         },
@@ -255,13 +329,14 @@ return {
             vehicleType = VehicleType.SEA,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Paleto Boathouse',
+                        sprite = 356,
+                        color = 3,
+                    },
                     coords = vec4(-277.4, 6637.01, 7.5, 40.51),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-289.2, 6637.96, 1.01, 45.5),
-                    blip = {
-                        name = 'Boathouse',
-                        sprite = 356,
-                    },
                 }
             },
         },
@@ -270,13 +345,14 @@ return {
             vehicleType = VehicleType.SEA,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Millars Boathouse',
+                        sprite = 356,
+                        color = 3,
+                    },
                     coords = vec4(1299.02, 4216.42, 33.91, 166.8),
                     size = vec3(10, 10, 10),
                     spawn = vec4(1296.78, 4203.76, 30.12, 169.03),
-                    blip = {
-                        name = 'Boathouse',
-                        sprite = 356,
-                    },
                 }
             },
         },
@@ -354,12 +430,14 @@ return {
             vehicleType = VehicleType.CAR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Impound Lot',
+                        sprite = 68,
+                        color = 3,
+                    },
                     coords = vec4(400.45, -1630.87, 29.29, 228.88),
                     size = vec3(10, 10, 10),
                     spawn = vec4(407.2, -1645.58, 29.31, 228.28),
-                    blip = {
-                        sprite = 68
-                    },
                 }
             },
         },
@@ -371,12 +449,14 @@ return {
             vehicleType = VehicleType.AIR,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'Air Depot',
+                        sprite = 359,
+                        color = 3,
+                    },
                     coords = vec4(-1244.35, -3391.39, 13.94, 59.26),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-1269.03, -3376.7, 13.94, 330.32),
-                    blip = {
-                        sprite = 359,
-                    },
                 }
             },
         },
@@ -388,12 +468,14 @@ return {
             vehicleType = VehicleType.SEA,
             accessPoints = {
                 {
+                    blip = {
+                        name = 'LSYMC Depot',
+                        sprite = 356,
+                        color = 3,
+                    },
                     coords = vec4(-772.71, -1431.11, 1.6, 48.03),
                     size = vec3(10, 10, 10),
                     spawn = vec4(-729.77, -1355.49, 1.19, 142.5),
-                    blip = {
-                        sprite = 356,
-                    },
                 }
             },
         },
