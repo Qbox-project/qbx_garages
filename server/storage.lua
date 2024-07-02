5c48c646badeb7fbf2237ba1db7b054242aec45a
Manason <clanerp@gmail.com>
2024-07-02 08:51:48 -0700

    refactor(server): use qbx_vehicles SaveVehicle export

--- server/storage.lua
+++ server/storage.lua
@@ -1,22 +1,6 @@
 ---@async
----@param vehicleId string
----@param props table ox_lib vehicle properties table
----@param garageName string
-local function saveVehicle(vehicleId, props, garageName)
-    MySQL.update('UPDATE player_vehicles SET state = ?, garage = ?, fuel = ?, engine = ?, body = ?, mods = ? WHERE id = ?', {VehicleState.GARAGED, garageName, props.fuelLevel, props.engineHealth, props.bodyHealth, json.encode(props), vehicleId})
-end
-
----@async
 local function moveOutVehiclesIntoGarages()
     MySQL.update('UPDATE player_vehicles SET state = ? WHERE state = ?', {VehicleState.GARAGED, VehicleState.OUT})
-end
-
----@async
----@param vehicleId integer
----@param depotPrice number
-local function setVehicleStateToOut(vehicleId, depotPrice)
-    local state = VehicleState.OUT
-    MySQL.update('UPDATE player_vehicles SET state = ?, depotprice = ? WHERE id = ?', {state, depotPrice, vehicleId})
 end
 
 ---@param vehicleId integer
@@ -43,9 +27,7 @@ local function setVehicleDepotPrice(vehicleId, depotPrice)
 end
 
 return {
-    saveVehicle = saveVehicle,
     moveOutVehiclesIntoGarages = moveOutVehiclesIntoGarages,
-    setVehicleStateToOut = setVehicleStateToOut,
     setVehicleGarage = setVehicleGarage,
     setVehicleDepotPrice = setVehicleDepotPrice,
 }
