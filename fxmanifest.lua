12001d96f6aa69270155938d84785ff697783513
Manason <Manason@users.noreply.github.com>
2024-07-08 17:46:39 +0000

    chore: bump manifest version to v1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
-version '1.0.2'
+version '1.1.0'
 
 name 'qbx_garages'
 description 'Garage system for Qbox'
 repository 'https://github.com/Qbox-project/qbx_garages'
-version '1.0.2'
+version '1.1.0'
 
 ox_lib 'locale'
 
